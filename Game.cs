96a5cf4d23e00d67736a97ad0a0553e3e5867935
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2020-09-14 17:02:20 -0500

    Added Player Class

--- Game.cs
+++ Game.cs
@@ -5,6 +5,13 @@ using System.Text;
 
 namespace TurnBasedPvPGame
 {
+    public struct Item
+    {
+        public string name;
+        public string description;
+        public int buff;
+    }
+
     class Game
     {
         bool _endGame = false;
@@ -13,25 +20,6 @@ namespace TurnBasedPvPGame
         Player _player1;
         Player _player2;
 
-        public struct Player
-        {
-            public string name;
-            public string combatClass;
-            public int maxHealth;
-            public int health;
-            public int level;
-            public int damage;
-            public int armor;
-            public Item item;
-        }
-
-        public struct Item
-        {
-            public string name;
-            public string description;
-            public string stat;
-            public int buff;
-        }
 
         public void Run()
         {
@@ -73,7 +61,7 @@ namespace TurnBasedPvPGame
                 InitPlayer(ref _player1, input, "Player 1");
 
                 Console.WriteLine();
-                Console.WriteLine("A " + _player1.combatClass + "! Good choice!");
+                Console.WriteLine("A " + _player1.GetCombatClass() + "! Good choice!");
                 Console.WriteLine();
 
                 Console.WriteLine("Player 2! Choose your character!");
@@ -83,12 +71,12 @@ namespace TurnBasedPvPGame
                 InitPlayer(ref _player2, input, "Player 2");
 
                 Console.WriteLine();
-                Console.WriteLine(_player2.combatClass + "! Perfect!");
+                Console.WriteLine(_player2.GetCombatClass() + "! Perfect!");
                 Console.WriteLine();
 
-                PrintPlayerStats(_player1);
+                _player1.PrintStats();
                 Console.WriteLine();
-                PrintPlayerStats(_player2);
+                _player2.PrintStats();
                 Console.WriteLine();
 
                 Console.WriteLine("Ready to begin?");
@@ -101,7 +89,7 @@ namespace TurnBasedPvPGame
             }
             else if (_gameState == "Game")
             {
-                if (_player1.health > 0 && _player2.health > 0)
+                if (_player1.IsAlive() && _player2.IsAlive())
                 {
                     Console.Clear();
                     Console.WriteLine("[Game]\n");
@@ -118,10 +106,10 @@ namespace TurnBasedPvPGame
             {
                 Console.Clear();
                 Console.WriteLine("[Game Over!]\n");
-                if (_player1.health > 0)
-                    Console.WriteLine(_player1.name + " wins!");
+                if (_player1.IsAlive())
+                    Console.WriteLine(_player1.GetName() + " wins!");
                 else
-                    Console.WriteLine(_player1.name + " wins!");
+                    Console.WriteLine(_player1.GetName() + " wins!");
 
                 _gameState = "Main Menu";
                 PressAnyKeyToContinue();
@@ -131,9 +119,9 @@ namespace TurnBasedPvPGame
         public void DoBattle()
         {
             // Display player stats
-            PrintPlayerStats(_player1);
+            _player1.PrintStats();
             Console.WriteLine();
-            PrintPlayerStats(_player2);
+            _player2.PrintStats();
             Console.WriteLine();
             // Player1 turn
             string player1Action = CombatMenu(_player1);
@@ -152,79 +140,47 @@ namespace TurnBasedPvPGame
         }
 
         #region COMBAT METHODS
-        public void AttackPlayer(ref Player player1, ref Player player2)
-        {
-            // Get damage against armor
-            int damage = player1.damage - player2.armor;
-            // No negative damage
-            player2.health -= Math.Max(0, damage);
-            // No negative health
-            Math.Clamp(player2.health, 0, player2.maxHealth);
-            Console.WriteLine(player1.name + " attacks " + player2.name + " dealing " + damage + " damage!");
-        }
-
-        public void ConsumeItem(ref Player player)
-        {
-            if (player.item.stat == "health")
-            {
-                player.health += player.item.buff;
-                Math.Clamp(player.health, 0, player.maxHealth);
-            }
-            else if (player.item.stat == "armor")
-            {
-                player.armor += player.item.buff;
-            }
-            else
-            {
-                Console.WriteLine(player.name + " checks their pockets, but there's nothing there!");
-                return;
-            }
-            Console.WriteLine(player.name + " consumes " + player.item.name + ". " + player.item.stat + " +" + player.item.buff);
-
-            player.item = InitItem("None");
-        }
-
         public void ResolveCombat(string player1Action, string player2Action)
         {
             // Player 1 turn
-            if (_player1.health > 0)
+            if (_player1.IsAlive())
             {
                 // Attack
                 if (player1Action == "1")
                 {
                     // Deal damage to player2
-                    AttackPlayer(ref _player1, ref _player2);
+                    _player1.Attack(_player2);
                 }
 
                 // Item usage
                 if (player1Action == "2")
                 {
-                    ConsumeItem(ref _player1);
+                    _player1.ConsumeItem();
                 }
             }
             else
             {
-                Console.WriteLine(_player1.name + " is dead!");
+                Console.WriteLine(_player1.GetName() + " is dead!");
             }
 
             // Player 2 turn
-            if (_player2.health > 0)
+            if (_player2.IsAlive())
             {
                 // Attack
                 if (player2Action == "1")
                 {
                     // Deal damage to player1
-                    AttackPlayer(ref _player2, ref _player1);
+                    _player2.Attack(_player1);
                 }
 
                 if (player2Action == "2")
                 {
-                    ConsumeItem(ref _player2);
+                    _player2.ConsumeItem();
                 }
             }
             else
             {
-                Console.WriteLine(_player2.name + " is dead!");
+                Console.WriteLine(_player2.GetName() + " is dead!");
             }
 
         }
@@ -232,7 +188,7 @@ namespace TurnBasedPvPGame
         public string CombatMenu(Player player)
         {
             Console.WriteLine();
-            Console.WriteLine(player.name);
+            Console.WriteLine(player.GetName());
             Console.WriteLine("Select an Action:");
             Console.WriteLine(" [1] Attack");
             Console.WriteLine(" [2] Use Item");
@@ -242,36 +198,16 @@ namespace TurnBasedPvPGame
         #endregion
 
         #region HELPERS
-        public void PrintPlayerStats(Player player)
-        {
-            Console.WriteLine(player.name + " Level " + player.level + " " + player.combatClass);
-            Console.WriteLine(player.health + "/" + player.maxHealth + "hp | " + player.damage + " atk | " + player.armor + " armor");
-        }
-
         // Initialize Player
         public void InitPlayer(ref Player player, string combatClass, string playerName)
         {
             if (combatClass == "1")
             {
-                player.name = playerName;
-                player.combatClass = "Knight";
-                player.maxHealth = 200;
-                player.health = player.maxHealth;
-                player.level = 1;
-                player.damage = 15;
-                player.armor = 10;
-                player.item = InitItem("Health Potion");
+                player = new Player(playerName, "Knight", 200, 1, 15, 10, InitItem("Health Potion"));
             }
             else
             {
-                player.name = playerName;
-                player.combatClass = "Wizard";
-                player.maxHealth = 100;
-                player.health = player.maxHealth;
-                player.level = 1;
-                player.damage = 50;
-                player.armor = 0;
-                player.item = InitItem("Armor Potion");
+                player = new Player(playerName, "Wizard", 100, 1, 50, 0, InitItem("Armor Potion"));
             }
         }
 
@@ -283,20 +219,17 @@ namespace TurnBasedPvPGame
             if (itemName == "Health Potion")
             {
                 item.buff = 20;
-                item.stat = "health";
-                item.description = "Health potion that heals " + item.buff + " " + item.stat;
+                item.description = "Health potion that heals " + item.buff + " Health";
 
             }
             else if (itemName == "Armor Potion")
             {
                 item.buff = 5;
-                item.stat = "armor";
-                item.description = "Armor potion that provides " + item.buff + " " + item.stat;
+                item.description = "Armor potion that provides " + item.buff + " Armor";
             }
             else
             {
                 item.buff = 0;
-                item.stat = "none";
                 item.description = "None";
             }
             return item;
---
+++ Player.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace TurnBasedPvPGame
+{
+    class Player
+    {
+        private string _name;
+        private string _combatClass;
+        private int _maxHealth;
+        private int _health;
+        private int _level;
+        private int _damage;
+        private int _armor;
+        private Item _item;
+
+        public Player()
+        {
+            _name = "None";
+            _combatClass = "None";
+            _maxHealth = 100;
+            _level = 1;
+            _damage = 10;
+            _armor = 0;
+        }
+
+        public Player(string name, string combatClass, int maxHealth, int level, int damage, int armor, Item item)
+        {
+            _name = name;
+            _combatClass = combatClass;
+            _maxHealth = maxHealth;
+            _health = maxHealth;
+            _level = level;
+            _damage = damage;
+            _armor = armor;
+            _item = item;
+        }
+
+        public string GetCombatClass()
+        {
+            return _combatClass;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public void TakeDamage(int damage)
+        {
+            damage -= _armor;
+            if (damage < 0)
+                damage = 0;
+
+            _health -= damage;
+            if (_health < 0)
+                _health = 0;
+
+            Console.WriteLine(_name + " took " + damage + " damage!");
+        }
+
+        public void Heal(int hp)
+        {
+            _health += hp;
+            if (_health > _maxHealth)
+                _health = _maxHealth;
+
+            Console.WriteLine(_name + " healed " + hp + " health!");
+        }
+
+        public void IncreaseArmor(int armor)
+        {
+            _armor += armor;
+        }
+        public void Attack(Player enemy)
+        {
+            enemy.TakeDamage(_damage);
+        }
+
+        public void ConsumeItem()
+        {
+            if (_item.name == "Health Potion")
+            {
+                Heal(_item.buff);
+            }
+            else if (_item.name == "Armor Potion")
+            {
+                IncreaseArmor(_item.buff);
+            }
+            else
+            {
+                return;
+            }
+            _item.name = "None";
+        }
+
+        public bool IsAlive()
+        {
+            if (_health > 0)
+                return true;
+            return false;
+        }
+        
+        public void PrintStats()
+        {
+            Console.WriteLine(_name + " Level " + _level + " " + _combatClass);
+            Console.WriteLine(_health + "/" + _maxHealth + "hp | " + _damage + " atk | " + _armor + " armor");
+        }
+    }
+}
